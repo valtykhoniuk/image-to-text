5a18a378109ed824248bbcff71ca7ca28086ec4f
valtykhoniuk <206243967+valtykhoniuk@users.noreply.github.com>
2026-08-15 21:45:09 +0200

    feat: set up Angular app with OCR page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OcrComponent } from './ocr/ocr.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, OcrComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('image-to-text');
-}
+export class App {}
---
+++ src/app/ocr/ocr.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ocr-component',
+  imports: [],
+  templateUrl: './ocr.component.html',
+  styleUrl: './ocr.component.css',
+})
+export class OcrComponent {}
